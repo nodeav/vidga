78e98820fe28393fb1346d35d70d94f32ca7f7d2
LosBunkos <nadavvadan@gmail.com>
2019-07-27 17:51:21 +0300

    add shape interface

--- interfaces/shapes/shape.h
+++ interfaces/shapes/shape.h
@@ -5,10 +5,29 @@
 #ifndef VIDGA_SHAPE_H
 #define VIDGA_SHAPE_H
 
+namespace vidga {
 
-class shape {
+    typedef struct coors {
+        unsigned int x;
+        unsigned int y;
+    } coors;
 
-};
+    template <class T>
+    class shape {
+    public:
+        // Getters
+        virtual coors getCenter() const = 0;
+        virtual unsigned int getWidth() const = 0;
+        virtual unsigned int getHeight() const = 0;
 
-
+        // Setters
+        virtual void setCenter(coors c) = 0;
+        virtual void setWidth(unsigned int newWidth) = 0;
+        virtual void setHeight(unsigned int newHeight) = 0;
+        virtual void draw(T& buffer) const = 0;
+    private:
+        coors center;
+        unsigned int width, height;
+    };
+}
 #endif //VIDGA_SHAPE_H
