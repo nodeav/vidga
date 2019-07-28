2c75b88ee4e377f0ed10fd02eee95a6015efb281
LosBunkos <nadavvadan@gmail.com>
2019-07-28 09:31:41 +0300

    remove draw method from shape interface - should be in the canvas interface/class

--- interfaces/shapes/shape.h
+++ interfaces/shapes/shape.h
@@ -24,7 +24,6 @@ namespace vidga {
         virtual void setCenter(coors c) = 0;
         virtual void setWidth(unsigned int newWidth) = 0;
         virtual void setHeight(unsigned int newHeight) = 0;
-        virtual void draw(T& buffer) const = 0;
     private:
         coors center;
         unsigned int width, height;
