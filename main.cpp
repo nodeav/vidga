959cfdbf935ad68315dc9493e8dc7dc482b01d34
LosBunkos <nadavvadan@gmail.com>
2019-08-05 09:04:38 +0300

    ui tweaks

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 using namespace vidga;
 
 int main() {
-    ucoor_t xRes = 640, yRes = 360;
+    ucoor_t xRes = 480, yRes = 270;
     const auto minRadius = static_cast<ucoor_t>((3.0/100) * xRes);
     const auto maxRadius = static_cast<ucoor_t>((10.0/100) * xRes);
     const float avgRadius = static_cast<ucoor_t>((minRadius + maxRadius) / 2);
@@ -26,15 +26,19 @@ int main() {
 
     for (auto i = 0; i < 3; i++) {
         auto canvas = cv::Mat(yRes, xRes, CV_8UC3, cv::Scalar(0, 0, 0));
-        std::string winName = "debug" + std::to_string(i);
+        std::string winName = "chromosome #" + std::to_string(i);
         std::cout << "using winName " << winName << std::endl;
-        cv::namedWindow(winName);
-        cv::moveWindow(winName, 300, 100);
+
         if (i < 2) {
             chromosomes.emplace_back(numCircles, minRadius, maxRadius, xRes, yRes);
         } else {
+            winName = "merged";
             chromosomes[0].mutRandMerge(chromosomes[1]);
         }
+
+        cv::namedWindow(winName);
+        cv::moveWindow(winName, xRes*(i%2), (yRes+50)*(i<2?1:2));
+
         std::cout << "Going to draw a " << chromosomes[i%2].getShapes().size() << "-long vector of shapes" << std::endl;
         // Weird things happen without this line
         chromosomes[i%2].draw(canvas, winName);
