1b7346ebc68e5817d2be0a515f929c9284c6d58f
Nadav Eidelstein <nadavvadan@gmail.com>
2020-04-11 17:16:38 +0300

    initial cuda impl

---
+++ cuda/cudaCircles.cu
@@ -0,0 +1,5 @@
+//
+// Created by nadav on 4/11/20.
+//
+
+#include "cudaCircles.cuh"
---
+++ cuda/cudaCircles.cuh
@@ -0,0 +1,14 @@
+//
+// Created by nadav on 4/11/20.
+//
+
+#ifndef VIDGA_CUDACIRCLES_CUH
+#define VIDGA_CUDACIRCLES_CUH
+
+
+class cudaCircles {
+
+};
+
+
+#endif //VIDGA_CUDACIRCLES_CUH
